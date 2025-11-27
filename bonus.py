0c3831c8feee016a155db80ebdaaad04ae93a48a
hemant17 <hemantbolashetti77@gmail.com>
2025-11-27 14:38:10 +0530

    Create bonus.py

---
+++ bonus.py
@@ -0,0 +1,17 @@
+import sys
+
+if len(sys.argv) != 5:
+    print("Usage: python student.py <name> <roll>")
+    sys.exit(1)
+
+script_name = sys.argv[0]
+salary = sys.argv[1]
+else:
+  script_name = sys.argv[0]
+ salary = 15000
+
+bamount=salary*0.10
+bonus=salary+bamount
+print("Bonus amount:",bamount)
+print("total Amount:",bonus
+ 
