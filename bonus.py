c15b08407eabb8b64462104d9d80467da6e22590
hemant17 <hemantbolashetti77@gmail.com>
2025-11-27 14:50:56 +0530

    Update bonus.py

--- bonus.py
+++ bonus.py
@@ -1,14 +1,11 @@
 import sys
 
-if len(sys.argv) != 5:
-    print("Usage: python student.py <name> <roll>")
-    sys.exit(1)
-
+if len(sys.argv) <3:
   script_name = sys.argv[0]
   salary = sys.argv[1]
 else:
   script_name = sys.argv[0]
- salary = 15000
+  salary = 15000
 
 bamount=salary*0.10
 bonus=salary+bamount
